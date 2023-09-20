808a2672419d764564d58c06358d244ab38bc9cc
riko-okubo <12riko14@gmail.com>
2023-09-20 22:58:44 +0900

    必要なapi作成

---
+++ frontend/src/api/get.tsx
@@ -0,0 +1,40 @@
+import { accessPointURL } from "./accessPoint";
+
+const getFolders = async (token: string) => {
+  const response = await fetch(`${accessPointURL}folders/?type=received`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+  });
+  const responseData = await response.json();
+  return responseData;
+};
+export { getFolders };
+
+const getTasks = async (folderId: string, token: string) => {
+  const response = await fetch(`${accessPointURL}task/folder_id=${folderId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+  });
+  const responseData = await response.json();
+  return responseData;
+};
+export { getTasks };
+
+const getTask = async (taskId: string, token: string) => {
+  const response = await fetch(`${accessPointURL}task/${taskId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+  });
+  const responseData = await response.json();
+  return responseData;
+};
+export { getTask };
