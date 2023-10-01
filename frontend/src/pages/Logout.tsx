3e777378e0d8916202b1c6f60fddc2b835ca158c
riko-okubo <12riko14@gmail.com>
2023-10-01 15:59:13 +0900

    不要なコード削除

--- frontend/src/pages/Logout.tsx
+++ frontend/src/pages/Logout.tsx
@@ -1,13 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "@chakra-ui/react";
-import { useAuth } from "../context/UserContext";
+import { useUser } from "../context/UserContext";
 
 const Logout = () => {
-  const { setAuth } = useAuth();
+  const { setUser } = useUser();
   const navigate = useNavigate();
 
   const onClickLogout = () => {
-    setAuth({ token: "" });
+    setUser({
+      id: 0,
+      username: "",
+      email: "",
+      is_staff: false,
+      is_superuser: false,
+      date_joined: "",
+      last_login: "",
+      company_id: 0,
+      position_id: 0,
+      count_emotions: 0,
+      count_comment: 0,
+    });
     navigate("/login");
   };
   return <Button onClick={onClickLogout}>ログアウト</Button>;
